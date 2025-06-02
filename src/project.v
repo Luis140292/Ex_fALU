008b329e60cbb8348c01e889144194413d747461
Luis140292 <luisbonilla.lmo@gmail.com>
2025-06-01 22:12:25 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,27 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
-`default_nettype none
-
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module tt_um_alu (
+    input  [7:0] ui_in,     // A[3:0], B[3:0]
+    output [7:0] uo_out,    // Resultado
+    input  [7:0] uio_in,    // Selector [2:0]
+    output [7:0] uio_out,   // Flags (si los usas)
+    output [7:0] uio_oe,    // Habilitación
+    input clk,
+    input rst_n
 );
+    wire [3:0] A = ui_in[3:0];
+    wire [3:0] B = ui_in[7:4];
+    wire [2:0] sel = uio_in[2:0];
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    wire [7:0] Y;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    alu_8bit alu_inst (
+        .A({4'b0000, A}),
+        .B({4'b0000, B}),
+        .sel(sel),
+        .Y(Y)
+    );
 
+    assign uo_out = Y;
+    assign uio_out = 8'b0; // Flags
+    assign uio_oe = 8'hFF; // Habilitar todas las salidas de uio
 endmodule
+
